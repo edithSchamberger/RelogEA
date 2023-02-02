cd21a6cadbec6ccc834c62ba54a3a8304e74665e
edithSchamby <g.edit.schamberger@gmail.com>
2023-02-02 08:51:33 -0300

    Add front requerimiento

---
+++ src/main/resources/static/app.component.ts
@@ -0,0 +1,7 @@
+import {Component ,OnInit} from '@angular/core';
+
+export class AppComponent implements OnInit{
+
+    public fechaminima:Date;
+    public fechamaxima: Date;
+}
--- src/main/resources/static/js/requerimientos.js
+++ src/main/resources/static/js/requerimientos.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+    $('#moda').attr('min', new Date().toISOString().split('T')[0])
+    //const modalFecha = document.querySelector("#modalFecha input")
+    //modalFecha.min = "2022-01-31"
+    // ver tema de minimo
+    //modalFecha.min = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`
+    console.log(modalFecha);
     cargarSolicitudes();
-
 
 
 });
@@ -8,12 +13,24 @@ $(document).ready(function() {
 const btnAgregarEfecto= document.querySelector("#ventana-emergente");
 const modal = document.querySelector("#modal");
 const btnVolver=document.querySelector("#volver")
+const btnAceptar=document.querySelector("#aceptar")
+const modalFecha = document.querySelector("#modalFecha");
+const btnVolverFecha=document.querySelector("#volverFecha")
+
 btnAgregarEfecto.addEventListener("click", ()=>{
     modal.showModal();
 })
 btnVolver.addEventListener("click", ()=>{
     modal.close();
 })
+
+btnAceptar.addEventListener("click", ()=>{
+    modalFecha.showModal();
+})
+btnVolverFecha.addEventListener("click", ()=>{
+    modalFecha.close();
+})
+
 
 const elementoSelector= document.getElementById("select-clase");
 elementoSelector.addEventListener("change", ()=>{
@@ -30,7 +47,9 @@ async function agregarSolicitudes(){
     console.log(efectosJson);
     let elementosHTML= [];
     for (let efecto of efectosJson){
-        let elementoEfecto = `<option>${efecto.nombreEfecto}</option>`
+        console.log("efecto");
+        console.log(efecto);
+        let elementoEfecto = `<option value=${efecto.efectoId}>${efecto.nombreEfecto}</option>`
         elementosHTML.push(elementoEfecto);
     }
     selectorEfecto.innerHTML= elementosHTML.join();
@@ -38,13 +57,10 @@ async function agregarSolicitudes(){
 
 function registrarSolicitudes(){
     let selectorEfecto= document.getElementById("select-efecto").value;
-    console.log("imprime 1");
     console.log(selectorEfecto);
     let cant = document.getElementById("cantidadEf").value;
-    console.log("imprime 2");
     console.log(cant);
     let efecID = selectorEfecto
-    console.log("imprime 3");
     console.log(efecID);
     let payload = {cantidad: cant  , efectoId: efecID }
     console.log(payload);
@@ -81,6 +97,34 @@ async function  cargarSolicitudes(){
     tabla.innerHTML = elementos.join("");
 
 }
+
+
+
+
+async function generarRequerimiento(){
+    const requerimientosReq = await fetch('/requerimientos');
+    const requerimientos = await requerimientosReq.json();
+    console.log(requerimientosReq);
+    console.log(requerimientos);
+    let elementos =[];
+    for (let req of requerimientos){
+        elementos.push(`<tr>
+            <td>${req.organizacion.organizacionId}</td>
+            <td>${req.fechaDeEntregaRequerida}</td>
+            <td>${req.solicitudes.solicitudId} </td>
+        </tr>`)
+    }
+    //let tabla=document.getElementById("Solicitudes-tbody");
+    //tabla.innerHTML = elementos.join("");
+
+    //{
+      //  "orgId": 1,
+        //"fechaDeEntregaRequerida": "2023-03-05T12:59:11.332",
+        //"idSolicitudes":[1,2]
+    //}
+}
+
+
 
 
 
